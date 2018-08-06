bd18ab1966bf3e65b35018b8dbef63999918c425
rowan <rjones@brookes.ac.uk>
2018-08-07 01:30:39 +0300

    initial commit - passing XCT

---
+++ TflChallenge/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TflChallenge
+//
+//  Created by Rowan on 06/08/2018.
+//  Copyright © 2018 Rowan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ TflChallenge/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  GlobalStruct.swift
+//  TflChallenge
+//
+//  Created by Rowan on 07/08/2018.
+//  Copyright © 2018 Rowan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let appId = "055186a1"
+    static let appKey = "aca4e20474fa9c8e204886a897332ab4"
+    
+    struct Messages {
+        static let notFoundError = "Road not found"
+        static let genericError = "whoops, something went wrong..."
+    }
+}
